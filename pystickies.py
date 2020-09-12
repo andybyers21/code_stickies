49b5a351cba43f122006512f426daa483bcc3fbb
andybyers21 <andy.byers21@gmail.com>
2020-09-12 14:36:59 +0100

    added menubar options

--- pystickies.py
+++ pystickies.py
@@ -1,6 +1,23 @@
 import tkinter as tk
 
-# Define the main functionality of the app.
+
+class Menubar:
+    def __init__(self, parent):
+        font_spec = ("", 14)
+
+        menubar = tk.Menu(parent.master, font=font_spec)
+        parent.master.config(menu=menubar)
+
+        file_dropdown = tk.Menu(menubar, font=font_spec, tearoff=0)
+        file_dropdown.add_command(label="New")
+        file_dropdown.add_command(label="Open")
+        file_dropdown.add_separator()
+        file_dropdown.add_command(label="Save")
+        file_dropdown.add_command(label="Save As")
+        file_dropdown.add_separator()
+        file_dropdown.add_command(label="Quit")
+
+        menubar.add_cascade(label="File", menu=file_dropdown)
 
 
 class Main:
@@ -9,11 +26,17 @@ class Main:
         master.title("Untitled")
         master.geometry("600x400")
 
-        self.textarea = tk.Text(master)
+        font_spec = ("", 20)
+
+        self.master = master
+
+        self.textarea = tk.Text(master, font=font_spec)
         self.scroll = tk.Scrollbar(master, command=self.textarea.yview)
         self.textarea.configure(yscrollcommand=self.scroll.set)
         self.textarea.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.menubar = Menubar(self)
 
 
 # Init root window

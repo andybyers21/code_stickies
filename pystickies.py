00ce37e19eaf8ec5e6d8fbd896f530ba32be5a07
andybyers21 <andy.byers21@gmail.com>
2020-09-12 14:01:18 +0100

    added scroll function

--- pystickies.py
+++ pystickies.py
@@ -9,6 +9,12 @@ class Main:
         master.title("Untitled")
         master.geometry("600x400")
 
+        self.textarea = tk.Text(master)
+        self.scroll = tk.Scrollbar(master, command=self.textarea.yview)
+        self.textarea.configure(yscrollcommand=self.scroll.set)
+        self.textarea.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
 
 # Init root window
 if __name__ == '__main__':
